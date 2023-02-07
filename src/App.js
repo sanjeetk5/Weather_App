51f86a615344579ce81467b522b96371800dc066
sanjeetk5 <112753484+sanjeetk5@users.noreply.github.com>
2023-02-07 14:30:52 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Weather from "./components/Weather";
+function App() {
+  return (
+    <div className="App">
+       <Weather />
+    </div>
+  );
+}
+
+export default App;
